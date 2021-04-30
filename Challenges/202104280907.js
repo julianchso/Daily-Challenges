151fea9e13c5364bd0f6cc4444a156c37137347d
Julian So <ch.julian.so@gmail.com>
2021-04-29 21:42:42 -0400

    attempt daily coding challenge

--- Challenges/202104280907.js
+++ Challenges/202104280907.js
@@ -1,34 +1,32 @@
 // https://www.codewars.com/kata/5629db57620258aa9d000014/train/javascript
 
-// The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum if this maximum is strictly greater than 1; 
+// The task is to produce a string in which each lowercase letters of s1 or s2 appears as many times as its maximum if this maximum is strictly greater than 1;
 // These letters will be prefixed by the number of the string where they appear with their maximum value and :.
 // If the maximum is in s1 as well as in s2 the prefix is =:.
 
 // example output: 1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg
 
-
 function mix(s1, s2) {
-  
+  s1obj = countLetters(s1);
+  s2obj = countLetters(s2);
 }
 
 function countLetters(str) {
-  letterStorage = {}
-  regex = /[a-z]/g
-  
+  letterStorage = {};
+  regex = /[a-z]/g;
+
   for (let i = 0; i < str.length; i++) {
-    // if (str[i] in letterStorage && str[i] == str[i].toLowerCase()) {
     if (str[i] in letterStorage && str[i].search(regex) === 0) {
-      // console.log(`lowercase`)
       letterStorage[str[i]]++;
     } else if (str[i].search(regex) === 0) {
-      // console.log(`firstLetter`)
       letterStorage[str[i]] = 1;
     }
   }
+  // Object.keys(letterStorage).sort();
   return letterStorage;
 }
 
-console.log(countLetters('AAA BBB bbb ccc'))
+console.log(mix("AAA BBB bbb ccc", "aa"));
 // console.log(countLetters('A BBB BB bbb c a'))
 // count number of lower case letters and store them in a dictionary.
 
