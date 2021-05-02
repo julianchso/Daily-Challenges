8454fc63777750fb88c7e84b751031fe57c847f0
Julian So <ch.julian.so@gmail.com>
2021-05-02 19:39:34 -0400

    almost finish 6kyu name rank problem

--- Challenges/202105021606.js
+++ Challenges/202105021606.js
@@ -3,33 +3,65 @@
 function rank(st, we, n) {
   let nameArr = st.split(",");
   let nameNum = {};
+  let nameScore = [];
+
   if (!st) {
     return "No participants";
   }
-  if (n > nameArr) {
-    return "";
+  if (n > nameArr.length) {
+    return "Not enough participants";
   }
 
   for (i = 0; i < nameArr.length; i++) {
     let nameSum = 0;
     for (j = 0; j < nameArr[i].length; j++) {
-      console.log(nameArr[i][j])
+      // console.log(nameArr[i][j])
       nameSum += nameArr[i][j].toLowerCase().charCodeAt(0) - 96;
+      // console.log(nameArr[i][j].toLowerCase().charCodeAt(0) - 96)
     }
+    nameSum += nameArr[i].length;
+    nameSum *= we[i];
     nameNum[nameArr[i]] = nameSum;
   }
 
-  console.log(nameNum);
+  // console.log(nameNum);
 
   for (i = 0; i < nameNum.length; i++) {
     nameNum[i] = nameNum[i] * we[i];
   }
+
+  for (i = 0; i < Object.keys(nameNum).length; i++) {
+    // console.log(nameNum[nameArr[i]])
+    nameScore.push(nameNum[nameArr[i]]);
+  }
+
+  nameScore.sort((a, b) => b - a);
+  // console.log(nameScore)
+
+  let nValue = nameScore[n - 1];
+  console.log(nValue);
+  let participant = Object.keys(nameNum).find((key) => nameNum[key] === nValue);
+  console.log(participant);
+  console.log(nameNum);
+  return participant;
 }
 
-// rank(
-//   "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin",
-//   [4, 2, 1, 4, 3, 1, 2],
-//   4
+// console.log(
+//   rank(
+//     "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin",
+//     [4, 2, 1, 4, 3, 1, 2],
+//     4
+//   )
 // );
+// // output: Benjamin
 
-rank("Paul", [4], 4);
+// console.log(rank("Lagon,Lily", [1, 5], 2))
+// // output: "Lagon"
+
+// console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8))
+// // output: "Not enough participants"
+
+// console.log(rank("", [4, 2, 1, 4, 3, 1, 2], 6))
+// // output: no participants
+
+console.log(rank("COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH", [1, 4, 4, 5, 2, 1], 1));
