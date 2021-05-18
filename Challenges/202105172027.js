76c99322a972d15e63acee9e299aa56c0fb282f0
Julian So <ch.julian.so@gmail.com>
2021-05-17 22:36:36 -0400

    doing 6kyu daily coding challenge

---
+++ Challenges/202105172027.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
+
+// function isPrime(num) {
+//   if (num <= 0 || num == 1) {
+//     return false;
+//   }
+//   if (num == 2) {
+//     return true;
+//   }
+//   if (num % 2 === 0) {
+//     return false;
+//   }
+//   for (let i = (num - 1)/2; i > 1; i--) {
+//     if (num % i == 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  if (num == 2) {
+    return true;
+  }
+  if (num % 2 === 0) {
+    return false;
+  }
+  for (let i = Math.ceil(Math.sqrt(num)); i > 1; i--) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
