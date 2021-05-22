90205b2a581a51e2a8cf9f8f0a33bbcf67daaa14
Julian So <ch.julian.so@gmail.com>
2021-05-22 11:10:59 -0400

    update daily coding challenge prime numbers

--- Challenges/202105172027.js
+++ Challenges/202105172027.js
@@ -1,23 +1,5 @@
 // https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
 
-// function isPrime(num) {
-//   if (num <= 0 || num == 1) {
-//     return false;
-//   }
-//   if (num == 2) {
-//     return true;
-//   }
-//   if (num % 2 === 0) {
-//     return false;
-//   }
-//   for (let i = (num - 1)/2; i > 1; i--) {
-//     if (num % i == 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
 function isPrime(num) {
   if (num < 2) {
     return false;
