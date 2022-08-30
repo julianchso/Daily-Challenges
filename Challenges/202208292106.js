64e76f055e83d901ac8fcbeef3c63bcd4a2007f3
Julian So <ch.julian.so@gmail.com>
2022-08-30 14:43:33 -0400

    finished and reviewed kata, codewars

--- Challenges/202208292106.js
+++ Challenges/202208292106.js
@@ -34,8 +34,50 @@ class User {
     this.progress = 0;
   }
 
-  
+  incProgress(activityLevel) {
+    if (activityLevel == this.rank) {
+      this.progress += 3;
+    }
 
+    if (activityLevel >= 9 || activityLevel <= -9 || activityLevel == 0) {
+      throw "Invalid paramaters!";
+    }
+
+    if (activityLevel > 0 && this.rank < 0) {
+      if (activityLevel - this.rank - 1 == -1) {
+        this.progress += 1;
+      }
+      if (activityLevel - this.rank - 1 <= -2) {
+        this.progress += 0;
+      } else {
+        let d = activityLevel - this.rank - 1;
+        this.progress += 10 * d * d;
+      }
+    } else {
+      if (activityLevel - this.rank + 1 == -1) {
+        this.progress += 1;
+      }
+      if (activityLevel - this.rank <= -2) {
+        this.progress += 0;
+      } else {
+        let d = activityLevel - this.rank;
+        this.progress += 10 * d * d;
+      }
+    }
+
+    while (this.progress >= 100) {
+      this.progress = this.progress - 100;
+      if (this.rank == -1) {
+        this.rank += 2;
+      } else {
+        this.rank++;
+      }
+    }
+    
+    if (this.rank == 8) {
+      return [(this.rank = 8), (this.progress = 0)];
+    }
+  }
 
   printRank() {
     console.log(this.rank);
