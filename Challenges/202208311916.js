6733e6a2238f4a36070052e450810520bce55ac8
Julian So <ch.julian.so@gmail.com>
2022-09-02 11:05:19 -0400

    finished Aug 31 daily challenge

--- Challenges/202208311916.js
+++ Challenges/202208311916.js
@@ -1,16 +1,18 @@
 // https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
 
-const numericTime = [];
-const InSeconds = {
-  year: 31536000,
-  day: 86400,
-  hour: 3600,
-  minute: 60,
-  second: 1,
-};
-const durationName = ["year", "day", "hour", "minute", "second"];
-
 function formatDuration(seconds) {
+  const numericTime = [];
+  const InSeconds = {
+    year: 31536000,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
+
+  if (seconds === 0) return "now";
+
+  const durationName = ["year", "day", "hour", "minute", "second"];
   for (duration in InSeconds) {
     durationNum = Math.floor(seconds / InSeconds[duration]);
 
@@ -22,28 +24,50 @@ function formatDuration(seconds) {
       seconds %= InSeconds[duration];
     }
   }
+  console.log(numericTime)
+
   str = "";
   numericTime.forEach((durationNum, i) => {
-    once = false;
-    // console.log(i);
-    // console.log(numericTime.length - 1);
     if (numericTime[i] == 0) {
     } else {
-      if (i != numericTime.length - 1) {
-        str += ", ";
-      } else {
-        str += " and ";
-      }
+      console.log(durationNum)
+
       str += durationNum + " " + durationName[i];
+      console.log(str)
       if (numericTime[i] > 1) {
         str += "s";
       }
+      str += ", "
     }
   });
-  str = str.substring(2, str.length);
+  str = str.substring(0, str.length - 2);
+  console.log(str);
+  str = str.replace(/,([^,]*)$/, " and" + '$1')
   console.log(numericTime);
   console.log(str);
   return str;
 }
 
-formatDuration(11231231702);
+formatDuration(62);
+
+function formatDuration(seconds) {
+  var time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+    res = [];
+
+  if (seconds === 0) return "now";
+
+  for (var key in time) {
+    if (seconds >= time[key]) {
+      var val = Math.floor(seconds / time[key]);
+      res.push((val += val > 1 ? " " + key + "s" : " " + key));
+      console.log(res)
+      seconds = seconds % time[key];
+    }
+  }
+
+  return res.length > 1
+    ? res.join(", ").replace(/,([^,]*)$/, " and" + "$1")
+    : res[0];
+}
+
+console.log(formatDuration(62));
